003c3120fc12bebfc9ad01a33edb42a117f9deec
Bobby0310 <83741230+Bobby0310@users.noreply.github.com>
2022-06-09 20:29:01 -0700

    Add files via upload

---
+++ made.js
@@ -0,0 +1,5 @@
+document.getElementById("Room_name").innerHTML="Hi,Welcome to the group -"+localStorage.getItem("room_name")+"- Enjoy ;)"
+
+function L(){
+    window.location="Kwitter_room.html"
+}
